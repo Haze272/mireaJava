b9a2c92557f2fb7e47b2054c4ddddd49fc497e08
Stepan Berkunov <55670335+Haze272@users.noreply.github.com>
2021-11-10 16:59:40 +0300

    task2 work with 2 elements

--- src/stepanPackage/practice6/task2/SortingStudentsByGPA.java
+++ src/stepanPackage/practice6/task2/SortingStudentsByGPA.java
@@ -28,27 +28,4 @@ public class SortingStudentsByGPA implements Comparator<Student> {
         else if (o1.getAverageScore() < o2.getAverageScore()) return -1;
         else return 0;
     }
-
-    public static void main(String[] args) {
-        SortingStudentsByGPA sorting;
-        Student student1 = new Student(55, "Pablo", 4.5);
-        Student student2 = new Student(13, "Juan", 3.56);
-        //Student student3 = new Student(35, "Aquilino", 4.67);
-        //Student student4 = new Student(568, "Nicolas", 4.93);
-        //Student student5 = new Student(5, "Rogelio", 3.7);
-        //Student student6 = new Student(39, "Iban", 2.72);
-        //Student student7 = new Student(81, "Fermin", 3.78);
-
-        System.out.println("Old array:");
-        for (Student std : Student.students) {
-            System.out.printf(std.toString());
-        }
-
-        ArrayList<Student> newStudents = sorting.fastSort(Student.students);
-
-        System.out.println("New array:");
-        for (Student std : Student.students) {
-            System.out.printf(std.toString());
-        }
-    }
 }
---
+++ src/stepanPackage/practice6/task2/TestSort.java
@@ -0,0 +1,31 @@
+package stepanPackage.practice6.task2;
+
+import stepanPackage.practice6.Student;
+
+import java.util.ArrayList;
+
+public class TestSort {
+    public static void main(String[] args) {
+        SortingStudentsByGPA sorting = new SortingStudentsByGPA();
+
+        Student student1 = new Student(55, "Pablo", 4.5);
+        Student student2 = new Student(13, "Juan", 3.56);
+        //Student student3 = new Student(35, "Aquilino", 4.67);
+        //Student student4 = new Student(568, "Nicolas", 4.93);
+        //Student student5 = new Student(5, "Rogelio", 3.7);
+        //Student student6 = new Student(39, "Iban", 2.72);
+        //Student student7 = new Student(81, "Fermin", 3.78);
+
+        System.out.println("Old array:");
+        for (Student std : Student.students) {
+            System.out.printf(std.toString());
+        }
+
+        ArrayList<Student> newStudents = sorting.fastSort(Student.students);
+
+        System.out.println("New array:");
+        for (Student std : Student.students) {
+            System.out.printf(std.toString());
+        }
+    }
+}
