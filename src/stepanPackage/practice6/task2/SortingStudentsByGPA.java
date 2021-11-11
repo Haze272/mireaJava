51e5f2c75f5e7a2fa7184b1fd2911c1fa38a30e9
Stepan Berkunov <55670335+Haze272@users.noreply.github.com>
2021-11-11 14:58:03 +0300

    Update SortingStudentsByGPA.java

--- src/stepanPackage/practice6/task2/SortingStudentsByGPA.java
+++ src/stepanPackage/practice6/task2/SortingStudentsByGPA.java
@@ -1,25 +1,45 @@
 package stepanPackage.practice6.task2;
 
 import stepanPackage.practice6.Student;
-
 import java.util.*;
 
 public class SortingStudentsByGPA implements Comparator<Student> {
 
+    ArrayList<Student> fastSort(ArrayList<Student> array, int leftBorder, int rightBorder) {
+        int leftMarker = leftBorder;
+        int rightMarker = rightBorder;
 
-    ArrayList<Student> fastSort(ArrayList<Student> array) {
-        if (array.size() < 2) return array;
-
-        if (array.size() == 2) {
-            if (compare(array.get(0), array.get(1)) > 0) {
-                Student toFirst = array.get(1);
-
-                array.set(1, array.get(0));
-                array.set(0, toFirst);
+        Student pivot = array.get((leftMarker + rightMarker) / 2);
+        do {
+            // Двигаем левый маркер слева направо пока элемент меньше, чем pivot
+            while (array.get(leftMarker) < pivot) {
+                leftMarker++;
             }
-        }
+            // Двигаем правый маркер, пока элемент больше, чем pivot
+            while (source[rightMarker] > pivot) {
+                rightMarker--;
+            }
+            // Проверим, не нужно обменять местами элементы, на которые указывают маркеры
+            if (leftMarker <= rightMarker) {
+                // Левый маркер будет меньше правого только если мы должны выполнить swap
+                if (leftMarker < rightMarker) {
+                    int tmp = source[leftMarker];
+                    source[leftMarker] = source[rightMarker];
+                    source[rightMarker] = tmp;
+                }
+                // Сдвигаем маркеры, чтобы получить новые границы
+                leftMarker++;
+                rightMarker--;
+            }
+        } while (leftMarker <= rightMarker);
 
-        return array;
+        // Выполняем рекурсивно для частей
+        if (leftMarker < rightBorder) {
+            fastSort(source, leftMarker, rightBorder);
+        }
+        if (leftBorder < rightMarker) {
+            fastSort(source, leftBorder, rightMarker);
+        }
     }
 
     @Override
